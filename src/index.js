cd1a6c7dc15c842b312ce13e8d016fb150389598
vipijth <vipijith007@gmail.com>
2021-11-24 12:33:24 +0530

    package

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
->>>>>>> Stashed changes
 import {
   BrowserRouter as Router,
   Routes,
@@ -20,21 +12,14 @@ import {
   useParams,
   NavLink,
   useNavigate,
-<<<<<<< Updated upstream
   useLocation
 } from "react-router-dom";
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
-  useLocation,
-} from "react-router-dom";
 
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "popper.js";
 import reportWebVitals from "./reportWebVitals";
->>>>>>> Stashed changes
 
 ReactDOM.render(
   <React.StrictMode>
