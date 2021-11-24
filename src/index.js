bc93e039c5f0b79cd9aab60698edd96c8638b8fa
vipijth <vipijith007@gmail.com>
2021-11-24 13:01:04 +0530

    compoments added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import {
   BrowserRouter as Router,
   Routes,
@@ -18,13 +18,16 @@ import {
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'popper.js';
-
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
