8ba6fe8d624417de49bd78f729e5db15896ddae4
vipijth <vipijith007@gmail.com>
2021-11-24 12:58:58 +0530

    componenets

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+  useParams,
+  NavLink,
+  useNavigate,
+  useLocation
+} from "react-router-dom";
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'popper.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
