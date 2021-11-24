0420078e3e9657e273c5cb6b0bcde88d77106cd6
vipijth <vipijith007@gmail.com>
2021-11-24 13:35:26 +0530

    imgslider and card

--- src/Components/ImgSlider.js
+++ src/Components/ImgSlider.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const ImgSlider = () => {
-    return <h3>This is ImgSlider row</h3>;
+    return (
+        <div className="row justify-content-around">
+            <div className="col-md-3 px-5 my-5 bg-dark slider_center_image" >
+                
+            </div>
+        </div>
+    );
 };
 
 export default ImgSlider;
--- src/Components/OurServices.js
+++ src/Components/OurServices.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React from "react";
 
 const OurServices = () => {
-    return <h3>This is OurServices row</h3>
+  return (
+    <div className="row row-cols-1 row-cols-md-2 g-4">
+      <div className="col-md-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Card title 1</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Card title 2</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Card title 3</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Card title 4</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OurServices;
