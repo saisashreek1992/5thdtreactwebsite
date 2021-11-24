abe6f875f85a24c848086998df08b31c1d9a77d5
saisashreek5thdt <sai@5thdt.com>
2021-11-24 11:25:34 +0530

    Added bootstrap package and configured in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+  useParams,
+  NavLink,
+  useNavigate,
+  useLocation
+} from "react-router-dom";
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
