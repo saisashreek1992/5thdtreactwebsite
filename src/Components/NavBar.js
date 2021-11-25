a9418a71c1a3275096f65430b49f30eb32cc8e57
vipijth <vipijith007@gmail.com>
2021-11-25 09:17:09 +0530

    navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-// 
+const nav_show=()=>{
+    document.body.classList.toggle('navigation-is-open');
+}
 
 
 const NavBar = () => {
     return (<div className="col-md-2  bg-dark navbar" >
-      	<a href="#cd-nav" class="cd-nav-trigger" > Menu 
+      	<a href="#cd-nav" class="cd-nav-trigger" onClick={nav_show}> Menu 
 		<span class="cd-nav-icon"></span>
 
 		<svg x="0px" y="0px" width="54px" height="54px" viewBox="0 0 54 54">
@@ -14,7 +16,7 @@ const NavBar = () => {
 	</a>
 	
 	<div id="cd-nav" class="cd-nav">
-		<div class="cd-navigation-wrapper">
+		<div class="cd-navigation-wrapper" id="cd-navigation-wrapper">
 			<div class="cd-half-block">
 				<h2 className="text-white" >Navigation</h2>
 
