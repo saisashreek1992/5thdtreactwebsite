45873a5bb937d0536fe54ce2c1339389e328946e
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2021-11-25 12:55:34 +0530

    Header.js updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className="row">
-            <div className="col px-5 my-5">
-                <h3>This is Header row</h3>
+        <div className="row align-items-start">
+            <div className="col px-5 my-3 align-self-start">
+                <div className="row position-relative align-items-start">
+                    <Link to="/home">
+                        <div className="col position-absolute align-self-start">
+                            <img src={Logo} className="img-fluid w-5 order-first" alt="5thdtlogo" />
+                        </div>
+                    </Link>
+                </div>
+                 
             </div>
         </div>
     );
