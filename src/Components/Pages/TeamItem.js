ee47b2b4914b4b81419c437a9c4a404f6370b062
saisashreek5thdt <sai@5thdt.com>
2021-11-26 14:35:52 +0530

    Img fixed to center in Team Section

--- src/Components/Pages/TeamItem.js
+++ src/Components/Pages/TeamItem.js
@@ -6,9 +6,9 @@ const TeamItem = (props) => {
     return (
         <div className="card">
             <div className="card-body">
-                <img src={logo} className="card-img-top team-img mx-auto" alt="logo" />
-                <h5 className="card-title">{props.teamName}</h5>
-                <p className="card-text">
+                <img src={logo} className="card-img-top team-img mx-auto d-block text-center" alt="logo" />
+                <h5 className="card-title text-center">{props.teamName}</h5>
+                <p className="card-text text-center">
                     {props.teamTag}
                 </p>
             </div>
