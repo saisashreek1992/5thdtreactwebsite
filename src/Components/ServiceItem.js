ea9ebecf88546a81b28b401f746cbbe0aa4b631b
saisashreek5thdt <sai@5thdt.com>
2021-11-29 13:09:31 +0530

    Commit

--- src/Components/ServiceItem.js
+++ src/Components/ServiceItem.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "../logo.svg";
+import '../Assets/styles.css';
 
 const ServiceItem = (props) => {
     return (
         <div className="card">
             <div className="card-body">
-                <img src={logo} className="card-img-top" alt="logo" />
+                <img src={logo} className="card-img-top team-img" alt="logo" />
                 <h5 className="card_sub_head">{props.serviceTitle}</h5>
                 <p className="card_para">
                     {props.serviceInfo}
