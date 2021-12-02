c7df7375197b69546eba04b26b309f49a8a197f6
saisashreek5thdt <sai@5thdt.com>
2021-12-02 12:59:45 +0530

    React-Bootstrap pluging added for contact-form

--- src/Components/Ui/Static Pages/ContactForm.js
+++ src/Components/Ui/Static Pages/ContactForm.js
@@ -1,58 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import {Form, Button}  from 'react-bootstrap';
+
 const ContactForm = () => {
+
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+    const fullNameChangeHandler = (event) => {
+        setFullName(event.target.value);
+    };
+
+    const emailChangeHandler = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const subjecChangeHandler = (event) => {
+        setSubject(event.target.value);
+    };
+
+    const messageChangeHandler = (event) => {
+        setMessage(event.target.value);
+    };
+
+    const contactSubmitHandler = (event) => {
+        event.preventDefault();
+        const contactForm = {
+            name: fullName,
+            email: email,
+            subject: subject,
+            info: message
+        };
+        console.log(contactForm);
+        setFullName('');
+        setEmail('');
+        setSubject('');
+        setMessage('');
+    };
+
     return (
-        <div className="row col px-5 my-3 ">
-            <form>
-                <div className="form-group ">
-                    <label for="fname">Full Name</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="fname"
-                        placeholder="Enter your Full Name"
+        <div className="row col px-5 my-3">
+            <Form onSubmit={contactSubmitHandler}>
+                <Form.Group>
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control 
+                        type='text'
+                        id='fname'
+                        placeholder="Enter Your Full Name"
+                        value={fullName}
+                        onChange={fullNameChangeHandler}
                     />
                     <small className="form-text text-muted"> &nbsp;</small>
-                </div>
-
-                <div className="form-group  ">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input
-                        type="email"
-                        className="form-control"
-                        id="exampleInputEmail1"
-                        aria-describedby="emailHelp"
-                        placeholder="Enter email"
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control 
+                        type='email'
+                        id='emailId'
+                        placeholder="Enter Your Email Address"
+                        value={email}
+                        onChange={emailChangeHandler}
                     />
                     <small id="emailHelp" className="form-text text-muted">
                         We'll never share your email with anyone else.
                     </small>
-                </div>
-                <div className="form-group ">
-                    <label for="sub">Subject</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="sub"
-                        placeholder="Enter Subject"
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Subject</Form.Label>
+                    <Form.Control 
+                        type='text'
+                        id='subject'
+                        placeholder="Enter Your Subject / Title"
+                        value={subject}
+                        onChange={subjecChangeHandler}
                     />
                     <small className="form-text text-muted"> &nbsp;</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1">
-                        Example textarea
-                    </label>
-                    <textarea
-                        class="form-control"
-                        id="exampleFormControlTextarea1"
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control 
+                        as="textarea"
                         rows="3"
-                    ></textarea>
+                        id='message'
+                        placeholder="Enter Your Message / Query"
+                        value={message}
+                        onChange={messageChangeHandler}
+                    />
                     <small className="form-text text-muted"> &nbsp;</small>
-                </div>
-
-                <button type="submit" className="btn btn-primary">
-                    Submit
-                </button>
-            </form>
+                </Form.Group>
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
         </div>
     );
 };
